9a4f51285cc8ccee22ca6e3529b7a5095301a763
madni <madnikhan.work@gmail.com>
2026-04-19 22:54:29 +0530

    Refactor data loading function to improve readability and maintainability

--- scr/data_pipeline.py
+++ scr/data_pipeline.py
@@ -1,23 +1,45 @@
 import pandas as pd
 from data_manager import BASE_DIR
 
-gdp_growth = BASE_DIR / "data-file" / "gdp growth.csv"
+gdp_growth_raw = BASE_DIR / "data-file" / "gdp growth.csv"
+inflation_raw = BASE_DIR / "data-file" / "inflation.csv"
+unemployment_raw = BASE_DIR / "data-file" / "Unemployment.csv"
 
 
 
-with open(gdp_growth) as f:
-    df=pd.read_csv(f,on_bad_lines='skip')
+def data_loader(file_path,skip_years=None,dropna=False):
+    
 
-df = df.rename(columns={
-    'ï»¿"Country Name"': 'country',
-    'Indicator Name': 'indicator'
-})
+    with open(file_path) as f:
+        df=pd.read_csv(f,on_bad_lines='skip')
+
+    df = df.rename(columns={
+        'ï»¿"Country Name"': 'country',
+        'Indicator Name': f'indicator({df["Indicator Name"].iloc[0]})' })
+    
+    df.drop(['Indicator Code','Country Code','2025','1960','Unnamed: 70'],axis=1,inplace=True)
+    
+    if skip_years !=None:
+        df.drop(skip_years,axis=1,inplace=True)
+
+    if dropna ==True:
+        df.dropna(inplace=True)
+        
 
 
 
-df.drop(['Indicator Code','Country Code'],axis=1,inplace=True)
-print(df.info())
+    return df
 
+
+gdp_growth_df = data_loader(gdp_growth_raw)
+inflation_df =data_loader(inflation_raw)
+unemployment = data_loader(unemployment_raw,'1962',True)
+
+
+
+# print(gdp_growth_df.info(),\
+#       inflation_df.info(),\
+#        unemployment.info())
 
 
 
